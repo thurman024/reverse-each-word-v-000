3a5bff6c94bdd3b425234f807662126f922f7745
Stephen Thurman <thurman024@gmail.com>
2018-01-24 22:31:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@ def reverse_each_word(string)
   array = string.split
   backwards = []
   array.collect do |word|
-    word.reverse
+    backwards << word.reverse
   end
-  require 'pry'
-  binding.pry
+  backwards.join(" ")
 end
